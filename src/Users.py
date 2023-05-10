fda2ba052a347f1c0a9947767b8824332ba40e3d
Simen Svenkerud <simen.svenkerud@storebrand.no>
2023-05-10 11:16:37 +0200

    Implementing user module

---
+++ src/Users.py
@@ -0,0 +1,61 @@
+class Users_Module:
+    """
+    Class for holding onto a permifrost users as imported from a spec file.
+
+    """
+
+    def __init__(self):
+        self.spesification = {}
+
+    def add_users(self, users):
+        """
+        Add users to the users object.
+        """
+        for user in users:
+            self.spesification.update(user)
+
+    def get_user(self, user):
+        """
+        Get a user from the users object.
+        """
+        if user not in self.spesification:
+            raise Exception("User not found")
+
+        return self.spesification[user]
+
+    def is_user(self, user):
+        """
+        Check if user is in the users object.
+        """
+        return user in self.spesification
+    
+    def __get_roles(self):
+        """
+        Get all roles from the users object.
+        """
+        roles = []
+        for user in self.spesification:
+            roles.extend(self.spesification[user]["member_of"])
+        return list(set(roles))
+    
+    def __get_login(self, yes_no):
+        """
+        Get all users that can login from the users object.
+        """
+        users = []
+        for user in self.spesification:
+            if self.spesification[user]["can_login"] == yes_no:
+                users.append(user)
+        return users
+    
+    def describe(self):
+        """
+        Provides a general description of the users object.
+        That can be used to validate the spesification.
+        """
+        self.count = len(self.spesification)
+        self.entities = list(self.spesification.keys())
+        self.roles = self.__get_roles()
+        self.can_login = self.__get_login(True)
+        self.cannot_login = self.__get_login(False)
+        return self
---
+++ tests/test_users.py
@@ -0,0 +1,82 @@
+import pytest
+import yaml
+from src.Users import Users_Module
+
+
+@pytest.fixture
+def user_object1():
+    return [
+        {"user1": {"can_login": True, "member_of": ["role1"]}},
+        {"user2": {"can_login": True, "member_of": ["role2"]}},
+    ]
+
+
+@pytest.fixture
+def user_object2():
+    return [
+        {"user3": {"can_login": False, "member_of": ["role3"]}},
+        {"user2": {"can_login": True, "member_of": ["role2"]}},
+    ]
+
+
+@pytest.fixture
+def users_loaded():
+    users = Users_Module()
+    users.spesification = {
+        "user1": {"can_login": True, "member_of": ["role1"]},
+        "user2": {"can_login": True, "member_of": ["role2"]},
+        "user3": {"can_login": False, "member_of": ["role3"]},
+    }
+    return users
+
+
+def test_user_add_users(user_object1):
+    users = Users_Module()
+    users.add_users(user_object1)
+    assert users.spesification == {
+        "user1": {"can_login": True, "member_of": ["role1"]},
+        "user2": {"can_login": True, "member_of": ["role2"]},
+    }
+
+
+def test_user_append_users(user_object1, user_object2, users_loaded):
+    users = Users_Module()
+    users.add_users(user_object1)
+    users.add_users(user_object2)
+    assert users.spesification == users_loaded.spesification
+
+
+def test_users_get_user(users_loaded):
+    assert users_loaded.get_user("user1") == {"can_login": True, "member_of": ["role1"]}
+
+
+def test_users_get_user_not_found(users_loaded):
+    with pytest.raises(Exception) as exception_info:
+        users_loaded.get_user("user4")
+    assert exception_info.type == Exception
+    assert exception_info.value.args[0] == "User not found"
+
+
+def test_users_is_user(users_loaded):
+    assert users_loaded.is_user("user1") == True
+
+def test_users_is_user_not_found(users_loaded):
+    assert users_loaded.is_user("user4") == False
+
+def test_users_describe(users_loaded):
+    user_description = users_loaded.describe()
+    assert user_description.count == 3
+    assert set(user_description.entities) == set(["user1", "user2", "user3"])
+    assert set(user_description.roles) == set(["role1", "role2", "role3"])
+    assert set(user_description.can_login) == set(["user1", "user2"])
+    assert set(user_description.cannot_login) == set(["user3"])
+
+def test_users_describe_empty():
+    users = Users_Module()
+    user_description = users.describe()
+    assert user_description.count == 0
+    assert user_description.entities == []
+    assert user_description.roles == []
+    assert user_description.can_login == []
+    assert user_description.cannot_login == []
+
