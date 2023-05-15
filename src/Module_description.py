453ce2cc2842aab806c752a4a5d13e8485e00431
Simen Svenkerud <simen.svenkerud@storebrand.no>
2023-05-15 13:53:25 +0200

    adding simple Description class for modules

---
+++ src/Module_description.py
@@ -0,0 +1,16 @@
+class Module_description:
+    def __init__(self):
+        self.type = ""
+        self.entities = []
+        self.count = 0
+        self.description = {}
+
+    def gather_description(self, module):
+        self.count = len(module.spesification)
+        self.entities = list(module.spesification.keys())
+        return self
+    
+    def return_description(self):
+        self.description["count"] = self.count
+        self.description["entities"] = self.entities
+        return self.description
---
+++ tests/test_module_description.py
@@ -0,0 +1,27 @@
+import pytest
+from src.Module_description import Module_description
+from src.Databases import Databases_Module
+
+
+@pytest.fixture
+def databases():
+    database = Databases_Module()
+    database.spesification = {
+        "database1": {"shared": False},
+        "database2": {"shared": False, "owner": "loader_qlik"},}
+    return database
+
+
+def test_module_description_gather_description(databases):
+    description = Module_description("databases")
+    description.gather_description(databases)
+    assert description.count == 2
+    assert description.entities == ["database1", "database2"]
+
+def test_module_description_return_description(databases):
+    description = Module_description("databases")
+    description.gather_description(databases)
+    assert description.return_description() == {
+        "count": 2,
+        "entities": ["database1", "database2"],
+    }
