df76601e35e998b7f46c71496b98c2dbe63551bc
Simen Svenkerud <simen.svenkerud@storebrand.no>
2023-05-15 14:59:08 +0200

    adding base class for modules to refactor

---
+++ src/Base_module.py
@@ -0,0 +1,17 @@
+class Base_Module:
+    def __init__(self):
+        self.spesification = {}
+        self.type = "Entity"
+
+    def add_entities(self, entities):
+        for entity in entities:
+            self.spesification.update(entity)
+
+    def get_entitiy(self, entity):
+        if self.is_entity(entity):
+            return self.spesification[entity]
+        else:
+            raise Exception(f"{self.type} not found")
+
+    def is_entity(self, entity):
+        return entity in self.spesification
