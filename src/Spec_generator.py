44d118a6d34d84011520baf754d60bca3c468860
Simen Svenkerud <simen.svenkerud@storebrand.no>
2023-05-16 15:46:51 +0200

    yaml generation of roels

--- src/Spec_generator.py
+++ src/Spec_generator.py
@@ -1,13 +1,13 @@
-
-class Spec_Generator():
+class Spec_Generator:
     def __init__(self, version):
         self.version = version
         self.output = ""
         self.users = "\n"
         self.databases = "\n"
         self.warehouses = "\n"
-        self.functional_roles = "\n"
-        self.access_roles = "\n"
+        self.functional_roles = ""
+        self.access_roles = ""
+        self.roles = "\n"
 
     def generate_users(self, module):
         self.users = "users:\n"
@@ -19,7 +19,9 @@ class Spec_Generator():
                     for role in module.spesification[user][key]:
                         self.users += f"""        - {role}\n"""
                 elif key == "can_login":
-                    self.users += f"""      {key}: {module.spesification[user][key]}\n"""
+                    self.users += (
+                        f"""      {key}: {module.spesification[user][key]}\n"""
+                    )
 
     def generate_databases(self, module):
         self.databases = "databases:\n"
@@ -27,30 +29,71 @@ class Spec_Generator():
             self.databases += f"""  - {database}:\n"""
             for key in module.spesification[database]:
                 if key == "owner":
-                    self.databases += f"""      {key}: {module.spesification[database][key]}\n"""
+                    self.databases += (
+                        f"""      {key}: {module.spesification[database][key]}\n"""
+                    )
                 elif key == "shared":
-                    self.databases += f"""      {key}: {module.spesification[database][key]}\n"""
+                    self.databases += (
+                        f"""      {key}: {module.spesification[database][key]}\n"""
+                    )
 
     def generate_warehouses(self, module):
         self.warehouses = "warehouses:\n"
         for warehouse in module.spesification:
             self.warehouses += f"""  - {warehouse}:\n"""
             for key in module.spesification[warehouse]:
-                self.warehouses += f"""      {key}: {module.spesification[warehouse][key]}\n"""
+                self.warehouses += (
+                    f"""      {key}: {module.spesification[warehouse][key]}\n"""
+                )
 
     def generate_roles(self, module):
-        self.functional_roles = "roles:\n"
+        self.roles = "roles:\n"
+        try:
+            self.__functional_role(module)
+            self.__access_role(module)
+        except:
+            if module.functional_roles != []:
+                self.__functional_role(module)
+            elif module.access_roles != []:
+                self.__access_role(module)
+            else:
+                raise Exception("No roles found in module")
+        
+        self.roles += f"{self.functional_roles}"
+        self.roles += f"{self.access_roles}"
+
+    def __access_role(self, module):
+        for access_role in module.access_roles:
+            self.access_roles += f"""  - {access_role}:\n"""
+            for key in module.spesification[access_role]:
+                if key == "privileges":
+                    self.access_roles += f"""    {key}:\n"""
+                    for privilege in module.spesification[access_role][key]:
+                        self.access_roles += f"""      {privilege}:\n"""
+                        for read_write in module.spesification[access_role][key][
+                            privilege
+                        ]:
+                            self.access_roles += f"""        {read_write}:\n"""
+                            for database in module.spesification[access_role][key][
+                                privilege
+                            ][read_write]:
+                                self.access_roles += (
+                                    f"""          - {database}\n"""
+                                )
+
+    def __functional_role(self, module):
         for functional_role in module.functional_roles:
             self.functional_roles += f"""  - {functional_role}:\n"""
             for key in module.spesification[functional_role]:
                 if key == "member_of":
-                    self.functional_roles += f"""      {key}:\n"""
+                    self.functional_roles += f"""    {key}:\n"""
                     for role in module.spesification[functional_role][key]:
-                        self.functional_roles += f"""        - {role}\n"""
+                        self.functional_roles += f"""      - {role}\n"""
                 elif key == "warehouses":
-                    self.functional_roles += f"""      {key}:\n"""
+                    self.functional_roles += f"""    {key}:\n"""
                     for warehouse in module.spesification[functional_role][key]:
-                        self.functional_roles += f"""        - {warehouse}\n"""
+                        self.functional_roles += f"""      - {warehouse}\n"""
+        self.functional_roles += "\n"
 
     def generate(self, module):
         self.output += f"""version: \"{self.version}\"\n"""
@@ -62,6 +105,5 @@ class Spec_Generator():
             self.generate_warehouses(module)
         elif module.type == "Role":
             self.generate_roles(module)
-
 
         self.output += self.users
--- tests/test_spec_generator.py
+++ tests/test_spec_generator.py
@@ -26,11 +26,13 @@ def singel_user_object():
     }
     return users
 
+
 @pytest.fixture
 def single_warehouse_object():
     warehouse = Warehouses_Module()
     warehouse.spesification = {"warehouse1": {"size": "xsmall"}}
     return warehouse
+
 
 @pytest.fixture
 def warehouses_object():
@@ -42,6 +44,7 @@ def warehouses_object():
     }
     return warehouses
 
+
 @pytest.fixture
 def single_database_object():
     database = Databases_Module()
@@ -50,7 +53,7 @@ def single_database_object():
 
 
 @pytest.fixture
-def databases_object(): 
+def databases_object():
     databases = Databases_Module()
     databases.spesification = {
         "database1": {"shared": "yes", "owner": "loader_qlik"},
@@ -59,21 +62,24 @@ def databases_object():
     }
     return databases
 
+
 @pytest.fixture
 def single_functional_role_object():
     functional_role = Roles_Module()
-    functional_role.spesification = {"role2": {
-                "warehouses": ["warehouse1"],
-                "member_of": [
-                    "ar_db_database1_r",
-                    "ar_db_database1_w",
-                    "ar_db_database2_r",
-                    "ar_db_database2_w",
-                ]
-            }
+    functional_role.spesification = {
+        "role2": {
+            "warehouses": ["warehouse1"],
+            "member_of": [
+                "ar_db_database1_r",
+                "ar_db_database1_w",
+                "ar_db_database2_r",
+                "ar_db_database2_w",
+            ],
+        }
     }
     functional_role.identify_roles()
     return functional_role
+
 
 @pytest.fixture
 def functional_roles_object():
@@ -89,21 +95,25 @@ def functional_roles_object():
             ]
         },
     }
+    functional_roles.identify_roles()
     return functional_roles
+
 
 @pytest.fixture
 def single_accsess_role_object():
     accsess_role = Roles_Module()
     accsess_role.spesification = {
-            "ar_db_database1_r": {
-                "privileges": {
-                    "databases": {"read": ["database1"]},
-                    "schemas": {"read": ["database1.*"]},
-                    "tables": {"read": ["database1.*.*"]},
-                }
+        "ar_db_database1_r": {
+            "privileges": {
+                "databases": {"read": ["database1"]},
+                "schemas": {"read": ["database1.*"]},
+                "tables": {"read": ["database1.*.*"]},
             }
         }
+    }
+    accsess_role.identify_roles()
     return accsess_role
+
 
 @pytest.fixture
 def accsess_roles_object():
@@ -118,9 +128,9 @@ def accsess_roles_object():
         },
         "ar_db_database1_w": {
             "privileges": {
-                "databases": {"read": ["database1"], "write": ["database1"]},
-                "schemas": {"read": ["database1.*"], "write": ["database1.*"]},
-                "tables": {"read": ["database1.*.*"], "write": ["database1.*.*"]},
+                "databases": { "write": ["database1"]},
+                "schemas": {"write": ["database1.*"]},
+                "tables": {"write": ["database1.*.*"]},
             }
         },
         "ar_db_database2_r": {
@@ -138,7 +148,55 @@ def accsess_roles_object():
             }
         },
     }
+    accsess_roles.identify_roles()
     return accsess_roles
+
+
+@pytest.fixture
+def roles_object():
+    roles = Roles_Module()
+    roles.spesification = {
+        "role1": {"member_of": ["role2"]},
+        "role2": {
+            "member_of": [
+                "ar_db_database1_r",
+                "ar_db_database1_w",
+                "ar_db_database2_r",
+                "ar_db_database2_w",
+            ]
+        },
+        "ar_db_database1_r": {
+            "privileges": {
+                "databases": {"read": ["database1"]},
+                "schemas": {"read": ["database1.*"]},
+                "tables": {"read": ["database1.*.*"]},
+            }
+        },
+        "ar_db_database1_w": {
+            "privileges": {
+                "databases": {"write": ["database1"]},
+                "schemas": {"write": ["database1.*"]},
+                "tables": {"write": ["database1.*.*"]},
+            }
+        },
+        "ar_db_database2_r": {
+            "privileges": {
+                "databases": {"read": ["database2"]},
+                "schemas": {"read": ["database2.*"]},
+                "tables": {"read": ["database2.*.*"]},
+            }
+        },
+        "ar_db_database2_w": {
+            "privileges": {
+                "databases": {"write": ["database2"]},
+                "schemas": {"write": ["database2.*"]},
+                "tables": {"write": ["database2.*.*"]},
+            }
+        },
+    }
+    roles.identify_roles()
+    return roles
+
 
 def test_spec_generator_singel_user_generator(singel_user_object):
     spec_generator = Spec_Generator("0.14.0")
@@ -157,34 +215,86 @@ def test_spec_generator_generate_users(users_object):
         == """users:\n  - user1:\n      can_login: yes\n      member_of:\n        - role1\n  - user2:\n      can_login: yes\n      member_of:\n        - role2\n  - user3:\n      can_login: no\n      member_of:\n        - role3\n"""
     )
 
+
 def test_spec_generator_generate_empty_users():
     users = Users_Module()
     spec_generator = Spec_Generator("0.14.0")
     spec_generator.generate(users)
     assert spec_generator.users == "users:\n"
 
+
 def test_spec_generator_generate_single_database(single_database_object):
     spec_generator = Spec_Generator("0.14.0")
     spec_generator.generate(single_database_object)
-    assert spec_generator.databases == """databases:\n  - database2:\n      shared: no\n      owner: loader_qlik\n"""
+    assert (
+        spec_generator.databases
+        == """databases:\n  - database2:\n      shared: no\n      owner: loader_qlik\n"""
+    )
+
 
 def test_spec_generator_generate_databases(databases_object):
     spec_generator = Spec_Generator("0.14.0")
     spec_generator.generate(databases_object)
-    assert spec_generator.databases == """databases:\n  - database1:\n      shared: yes\n      owner: loader_qlik\n  - database2:\n      shared: no\n      owner: loader_qlik\n  - database3:\n      shared: yes\n"""
+    assert (
+        spec_generator.databases
+        == """databases:\n  - database1:\n      shared: yes\n      owner: loader_qlik\n  - database2:\n      shared: no\n      owner: loader_qlik\n  - database3:\n      shared: yes\n"""
+    )
+
 
 def test_spec_generator_generate_single_warehouse(single_warehouse_object):
     spec_generator = Spec_Generator("0.14.0")
     spec_generator.generate(single_warehouse_object)
-    assert spec_generator.warehouses == """warehouses:\n  - warehouse1:\n      size: xsmall\n"""
+    assert (
+        spec_generator.warehouses
+        == """warehouses:\n  - warehouse1:\n      size: xsmall\n"""
+    )
+
 
 def test_spec_generator_generate_warehouses(warehouses_object):
     spec_generator = Spec_Generator("0.14.0")
     spec_generator.generate(warehouses_object)
-    assert spec_generator.warehouses == """warehouses:\n  - warehouse1:\n      size: xsmall\n  - warehouse2:\n      size: xsmall\n  - warehouse3:\n      size: medium\n"""
+    assert (
+        spec_generator.warehouses
+        == """warehouses:\n  - warehouse1:\n      size: xsmall\n  - warehouse2:\n      size: xsmall\n  - warehouse3:\n      size: medium\n"""
+    )
 
 
 def test_spec_generator_generate_single_functional_role(single_functional_role_object):
     spec_generator = Spec_Generator("0.14.0")
     spec_generator.generate(single_functional_role_object)
-    assert spec_generator.functional_roles == """roles:\n  - role2:\n      warehouses:\n        - warehouse1\n      member_of:\n        - ar_db_database1_r\n        - ar_db_database1_w\n        - ar_db_database2_r\n        - ar_db_database2_w\n""" 
+    assert (
+        spec_generator.functional_roles
+        == """  - role2:\n    warehouses:\n      - warehouse1\n    member_of:\n      - ar_db_database1_r\n      - ar_db_database1_w\n      - ar_db_database2_r\n      - ar_db_database2_w\n\n""")
+
+def test_spec_generator_generate_multiple_functional_roles(functional_roles_object):
+    spec_generator = Spec_Generator("0.14.0")
+    spec_generator.generate(functional_roles_object)
+    assert (
+        spec_generator.functional_roles
+        == """  - role1:\n    member_of:\n      - role2\n  - role2:\n    member_of:\n      - ar_db_database1_r\n      - ar_db_database1_w\n      - ar_db_database2_r\n      - ar_db_database2_w\n\n""")
+
+
+def test_spec_generator_generate_accsess_roles(single_accsess_role_object):
+    spec_generator = Spec_Generator("0.14.0")
+    spec_generator.generate(single_accsess_role_object)
+    assert (
+        spec_generator.access_roles
+        == """  - ar_db_database1_r:\n    privileges:\n      databases:\n        read:\n          - database1\n      schemas:\n        read:\n          - database1.*\n      tables:\n        read:\n          - database1.*.*\n""" 
+          )
+
+
+def test_spec_generator_generate_multiple_accsess_roles(accsess_roles_object):
+    spec_generator = Spec_Generator("0.14.0")
+    spec_generator.generate(accsess_roles_object)
+    assert (
+        spec_generator.access_roles
+        =="""  - ar_db_database1_r:\n    privileges:\n      databases:\n        read:\n          - database1\n      schemas:\n        read:\n          - database1.*\n      tables:\n        read:\n          - database1.*.*\n  - ar_db_database1_w:\n    privileges:\n      databases:\n        write:\n          - database1\n      schemas:\n        write:\n          - database1.*\n      tables:\n        write:\n          - database1.*.*\n  - ar_db_database2_r:\n    privileges:\n      databases:\n        read:\n          - database2\n      schemas:\n        read:\n          - database2.*\n      tables:\n        read:\n          - database2.*.*\n  - ar_db_database2_w:\n    privileges:\n      databases:\n        write:\n          - database2\n      schemas:\n        write:\n          - database2.*\n      tables:\n        write:\n          - database2.*.*\n""" 
+    )
+
+def test_spec_generator_generate_roles(roles_object):
+    spec_generator = Spec_Generator("0.14.0")
+    spec_generator.generate(roles_object)
+    assert (
+        spec_generator.roles
+        =="""roles:\n  - role1:\n    member_of:\n      - role2\n  - role2:\n    member_of:\n      - ar_db_database1_r\n      - ar_db_database1_w\n      - ar_db_database2_r\n      - ar_db_database2_w\n\n  - ar_db_database1_r:\n    privileges:\n      databases:\n        read:\n          - database1\n      schemas:\n        read:\n          - database1.*\n      tables:\n        read:\n          - database1.*.*\n  - ar_db_database1_w:\n    privileges:\n      databases:\n        write:\n          - database1\n      schemas:\n        write:\n          - database1.*\n      tables:\n        write:\n          - database1.*.*\n  - ar_db_database2_r:\n    privileges:\n      databases:\n        read:\n          - database2\n      schemas:\n        read:\n          - database2.*\n      tables:\n        read:\n          - database2.*.*\n  - ar_db_database2_w:\n    privileges:\n      databases:\n        write:\n          - database2\n      schemas:\n        write:\n          - database2.*\n      tables:\n        write:\n          - database2.*.*\n""" 
+    )
