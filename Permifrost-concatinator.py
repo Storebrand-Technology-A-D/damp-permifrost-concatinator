081b5411c6a2b1f6a7cf8077bbb05ca1d9a2d1cf
Simen Svenkerud <simen.svenkerud@storebrand.no>
2023-06-01 15:59:56 +0200

    setting up simple CLI interface

---
+++ Permifrost-concatinator.py
@@ -0,0 +1,21 @@
+import click
+from src.Spesification import Spesification
+
+
+@click.command()
+@click.option("--input", help="Input file or directory", required=True)
+@click.option("--output", help="Output path and filename", required=True)
+@click.option(
+    "--verification", help="Verify the spesification", required=False, default=False
+)
+def main(input, output, verification):
+    spesification = Spesification(verification=verification)
+    spesification.load(input)
+    spesification.identify_modules()
+    spesification.identify_entities()
+    spesification.generate()
+    spesification.export(output)
+
+
+if __name__ == "__main__":
+    main()
