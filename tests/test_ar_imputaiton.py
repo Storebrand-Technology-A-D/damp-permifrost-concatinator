7529d9346e30565a86350ef7591462c75fb04a0a
Simen Svenkerud <simen.svenkerud@storebrand.no>
2023-07-19 15:29:30 +0200

    Cleanup

--- tests/test_ar_imputaiton.py
+++ tests/test_ar_imputaiton.py
@@ -1,10 +1,3 @@
-import pytest 
-import os
-import logging
-
-from src.permifrost_concatinator.Roles_module import Roles_Module
-from src.permifrost_concatinator.Databases_module import Databases_Module
-
 def test_generate_ar_from_db(databases_object, accsess_roles_object):
     databases_object.spesification.pop("database3") # removed to match accsess_roles_object.spesification
     accsess_roles = databases_object.generate_accsess_roles()
