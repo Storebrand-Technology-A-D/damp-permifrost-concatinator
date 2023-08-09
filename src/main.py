68f8e90d6ba401f1f51e813331fc8499de9f1a3c
Simen Svenkerud <simen.svenkerud@storebrand.no>
2023-08-09 13:47:16 +0200

    adding state storrage to permifrost CLI

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import click
 import logging
 from permifrost_concatinator.Spesification import Spesification
+from permifrost_concatinator.Permission_state import Permission_state
+from permifrost_concatinator.loader_local_file import Local_file_loader
 
 
 @click.command()
@@ -9,7 +11,11 @@ from permifrost_concatinator.Spesification import Spesification
 @click.option(
     "--verification", help="Verify the spesification", required=False, default=False
 )
-def main(input, output, verification):
+@click.option("--plan",help="output planned changes based on spec comparison", required=False, default=False )
+@click.option("--apply",help="apply planned changes based on spec comparison", required=False, default=False )
+@click.option("--state",help="path to state file", required=False, default=False )
+
+def main(input, output, verification, plan, apply, state):
     logformat = logging.Formatter(fmt="%(levelname)s - %(message)s")
     consolHandler = logging.StreamHandler()
     consolHandler.setLevel(logging.INFO)
@@ -23,8 +29,27 @@ def main(input, output, verification):
     spesification.identify_modules()
     spesification.identify_entities()
     spesification.generate()
+    if plan==True:
+        try:
+            current_state = Permission_state()
+            current_state.load(Local_file_loader(), state)
+        except:
+            logger.error("Failed to load state")
+
+        new_state = Permission_state(spesification).generate()
+        new_state.compare(state)
+        new_state.plan()
+
+
     spesification.export(output)
 
+    if apply==True:
+        try:
+            new_state = Permission_state(spesification).generate()
+            new_state.compare(state)
+            new_state.export(state)
+        except:
+            logger.error("Failed to update state file")
 
 if __name__ == "__main__":
     main()
